04f1b991dd0c7c1df36af10526033e6d7d41673c
F5T3 <uccchipofya@gmail.com>
2024-07-16 10:12:58 +0200

    Sent local PowerShell profile updates to GitHub

---
+++ sync.ps1
@@ -0,0 +1,12 @@
+param (
+    [string]$commitMessage = "Sent local PowerShell profile updates to GitHub"
+)
+
+$profileDir = Split-Path -Path $PROFILE
+Set-Location -Path $profileDir
+
+git add .
+
+git commit -m $commitMessage
+
+git push origin main
